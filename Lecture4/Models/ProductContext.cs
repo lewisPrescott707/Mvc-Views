adbeb31c2fb85b6f46a1bd5b2a6d805cdb7fd9d6
Lewis Prescott <lewis.prescott@asos.com>
2018-02-14 12:14:21 +0000

    Add ProductsController

---
+++ Lecture4/Models/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lecture4.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
